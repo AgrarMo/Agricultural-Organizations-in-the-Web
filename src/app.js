9c7e098fe9be64a44455760c2dbe5a5fa4c0a62b
AgrarMo <moritzmax.maurer@gmail.com>
2025-02-10 18:26:56 +0100

    Added some text

--- src/app.js
+++ src/app.js
@@ -6,13 +6,11 @@ function App() {
   return (
     <div className="App">
       <div className="header-container">
-        <div classname="description1">
-          <h1>Suisse Agricultural Organizations in the Webtestetst</h1>
+        <div classname="header">
+          <h1>Suisse Agricultural Organizations in the Web</h1>
         </div>
-        <div classname="description2">
+        <div classname="descriptionheader1">
         <p> This plot displays 1.734 agricultural websites (green), their interlinkages, and their interlinkages with websites of non-agricultural organizations (pink). Explore this webscape to get an bubble-indipendent impression of the versatility of suisse agriculture. The crawler is still running and data regularly updated.  </p>
-        </div>
-        <div classname="description3">
         </div>
       </div> 
       <div className="graph-container">
