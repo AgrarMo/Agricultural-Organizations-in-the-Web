85e37a624f34c469931a5f4d292519914c85727e
AgrarMo <moritzmax.maurer@gmail.com>
2025-02-25 12:22:16 +0100

    Added readme and Licence

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ function App() {
           <h1>The Swiss Agricultural Webscape</h1>
         </div>
         <div className="descriptionheader1">
-        <p> This plot displays 5'792 Swiss agricultural websites (green) and their interlinkages. 
+        <p> This plot displays 5,792 Swiss agricultural websites (green) and their interlinkages. 
           Explore this webscape to get a bubble-indipendent impression of the versatility of Swiss agriculture. 
           Or use the search field to select a website and study what other websites refer to it. 
           The crawler that generated the data has identified some non-agricultural websites as agricultural websites (approximately 3 out of 10). I am working on improving the crawler's accuracy.
